4e7fd02fb2fff33d4b00414645f4680aa55b6b7c
sikander27 <imsikandarkhan@outlook.com>
2024-02-29 21:59:09 +0530

    final

--- server.py
+++ server.py
@@ -3,29 +3,7 @@ import asyncio
 
 import os
 import json
-import time
 
-Messages = [
-    "Hey",
-    "Thank God We did it",
-    "Let's take to next level"
-]
-
-
-# def read_last_n_lines(n, file_path='log.txt'):
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         file.seek(0, os.SEEK_END)
-#         file_size = file.tell()
-#         block_size = 1024
-#         lines = []
-
-#         while len(lines) < n and file.tell() > 0:
-#             seek_position = min(file_size, file.tell() - block_size)
-#             file.seek(seek_position)
-#             block = file.read(block_size)
-#             lines.extend(reversed(block.splitlines()))
-#             file_size -= block_size
-#         return lines[-n:]
 
 def read_last_n_lines(n, file_path='log.txt'):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -51,15 +29,12 @@ def get_last_line(file_path='log.txt'):
         last_line = file.tell()
 
         return last_line
-    
+
 
 async def handler(websocket):
-    # "Read file"
     logs, last_line = read_last_n_lines(10)
     print(f"Last line is {last_line}")
-    # import pdb; pdb.set_trace()
     for message in logs:
-        # print(f"sending msg: {message}...")
         event = {
                 "type": "new",
                 "message": message,
@@ -69,7 +44,7 @@ async def handler(websocket):
         if get_last_line() > last_line:
             logs, last_line = read_last_n_lines(1)
             for message in logs:
-                # print(f"sending msg: {message}...")
+                print(message)
                 event = {
                         "type": "new",
                         "message": message,
@@ -80,22 +55,10 @@ async def handler(websocket):
 
 async def main():
     async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+        await asyncio.Future()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
 
 
-
-
-
-
-# def read_last_n_lines_old(N, fname='log.txt'):
-#     lines = []
-#     with open(fname) as file:
-#         for line in (file.readlines() [-N:]):
-#         # for line in (file.readlines() [-N:]):
-#             lines.append(str(line))
-#     print(f"Last 10 logs {lines}")
-#     return lines
